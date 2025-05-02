6e3992a0245c658044cc8b05a4f090e3468f03b1
Ransom Kwelle <ransom_gentle@yahoo.com>
2025-05-02 13:58:56 -0700

    feat: Add basic structure and router plumbing

---
+++ cmd/api/health.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	// Set the content type to application/json
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write a simple JSON response
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"status":"ok"}`))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rkwelle/social-app
+
+go 1.23.2
+
+require github.com/go-chi/chi/v5 v5.2.1 // indirect
