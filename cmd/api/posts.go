825cf32090fd120d0406194bf280f767e701c13c
Ransom Kwelle <ransom_gentle@yahoo.com>
2025-05-14 18:02:21 -0700

    feat: Standardize json response to client

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -53,7 +53,7 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+	if err := app.jsonResponse(w, http.StatusCreated, post); err != nil {
 		// writeJSONError(w, http.StatusInternalServerError, err.Error())
 		app.internalServerError(w, r, err)
 		return
@@ -97,7 +97,7 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 
 	post.Comments = comments
 
-	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+	if err := app.jsonResponse(w, http.StatusCreated, post); err != nil {
 		// writeJSONError(w, http.StatusInternalServerError, err.Error())
 		app.internalServerError(w, r, err)
 		return
@@ -162,7 +162,7 @@ func (app *application) updatePostHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	if err := writeJSON(w, http.StatusOK, post); err != nil {
+	if err := app.jsonResponse(w, http.StatusOK, post); err != nil {
 		// writeJSONError(w, http.StatusInternalServerError, err.Error())
 		app.internalServerError(w, r, err)
 		return
