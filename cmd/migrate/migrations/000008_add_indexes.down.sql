715bc71d8d51bb62b1e4e3f69527b92f20e5dbd7
Ransom Kwelle <ransom_gentle@yahoo.com>
2025-05-24 21:11:28 -0700

    feat: add indexes for full-text search

---
+++ cmd/migrate/migrations/000008_add_indexes.down.sql
@@ -0,0 +1,8 @@
+DROP INDEX IF NOT EXISTS idx_comments_content;
+
+DROP INDEX IF NOT EXISTS idx_posts_title;
+DROP INDEX IF NOT EXISTS idx_posts_tags;
+
+DROP INDEX IF NOT EXISTS idx_users_username;
+DROP INDEX IF NOT EXISTS idx_posts_user_id;
+DROP INDEX IF NOT EXISTS idx_comments_post_id;
