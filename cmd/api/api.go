731e163d5f915df84127517f0f9df505a6d9c2df
Ransom Kwelle <ransom_gentle@yahoo.com>
2025-05-26 14:57:37 -0700

    feat: add user feed algrithm

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -61,6 +61,10 @@ func (app *application) mount() *chi.Mux {
 				r.Put("/follow", app.followUserHandler)
 				r.Put("/unfollow", app.unfollowUserHandler)
 			})
+
+			r.Group(func(r chi.Router) {
+				r.Get("/feed", app.getUserFeedHandler)
+			})
 		})
 	})
 
