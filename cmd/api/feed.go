6c3d7abe11184b1858c4d0ca66a33623706548e1
Ransom Kwelle <ransom_gentle@yahoo.com>
2025-05-31 15:46:02 -0700

    feat: add pagination to feed

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/rkwelle/social-app/internal/store"
 )
 
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
-	// pagination, filters
+
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+	}
+
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := validate.Struct(fq); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
 
 	ctx := r.Context()
 
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(20))
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(20), fq)
 	if err != nil {
 		app.internalServerError(w, r, err)
 		return
